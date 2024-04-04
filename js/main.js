9a3174a11244293917f425247505e689585a4d99
VITÓRIA CARDOSO COSTA <23111831@CFP127ALUNO.LOCAL>
2024-04-04 12:23:26 -0300

    em andamento

--- js/main.js
+++ js/main.js
@@ -1,31 +1,44 @@
-'use strict'
-
-import { getFilmes, getFilme } from "./filmes.js"
+import { getFilmes, getFilme, postFilme } from "./filmes.js"
 
 function criarCard(filme) {
+    
     const card = document.createElement('div')
     const titulo = document.createElement('h2')
+    const valor = document.createElement('button')
+    const capa_filme = document.createElement('img')
+    valor.textContent = filme.valor_unitario
     titulo.textContent = filme.nome
-    const texto = document.createElement('textarea')
-    texto.textContent = filme.sinopse
-
-    card.append(titulo, texto)
+    capa_filme.src = filme.foto_capa
+ 
+    // card.classList.add(
+    //     'flex'
+    // )
+    capa_filme.classList.add(
+        'h-56',
+        'mx-px',
+        'rounded-md',
+        'border-blue-800',
+        'flex',
+        'flex-row'
+    ) 
+    card.append(capa_filme, titulo)
 
     return card
 }
+// console.log(card)
 
-async function preencherContainer() {
+async function preencherContainer(){
+    
     const container = document.querySelector('body')
-
     const filmes = await getFilmes()
 
     filmes.forEach(filme => {
-        
         const card = criarCard(filme)
-        container.append(card)
-        console.log(card)
+        container.appendChild(card)
     })
     
 }
-preencherContainer()
 
+function
+
+preencherContainer()
